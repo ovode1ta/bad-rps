ed574970191287a268397d8d68a5fe34a643452f
cgdisgood <71853433+cgdisgood@users.noreply.github.com>
2020-09-24 17:06:40 -0500

    Create bad-rps.py

---
+++ bad-rps.py
@@ -0,0 +1,19 @@
+choice = input("Rock, Paper, or Scissors? ")
+
+if choice == "Rock":
+    print("I choose Paper! \nI win!")
+
+if choice == "Paper":
+    print("I choose Scissors! \nI win!")
+
+if choice == "Scissors":
+    print("I choose Rock! \nI win!")
+
+if choice == "rock":
+    print("I choose Paper! \nI win!")
+
+if choice == "paper":
+    print("I choose Scissors! \nI win!")
+
+if choice == "scissors":
+    print("I choose Rock! \nI win!")
